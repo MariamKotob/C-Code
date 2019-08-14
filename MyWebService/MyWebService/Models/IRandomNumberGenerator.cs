d21ee1f4288c55326e80c7305d69a18025102e3b
Mariam <memoabdo2036@gmail.com>
2019-08-14 22:29:13 +0200

    Implement a simple web service

---
+++ MyWebService/MyWebService/Models/IRandomNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWebService.Models
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRandomNumberGenerator" in both code and config file to gether.
+    [ServiceContract]
+    public interface IRandomNumberGenerator 
+    {
+        [OperationContract]
+        NumberModel GetRandomNumbers();
+    }
+}
---
+++ MyWebService/MyWebService/Models/RandomNumberGenerator.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWebService.Models
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RandomNumberGenerator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RandomNumberGenerator.svc or RandomNumberGenerator.svc.cs at the Solution Explorer and start debugging.
+    public class RandomNumberGenerator : IRandomNumberGenerator
+    {
+        public NumberModel GetRandomNumbers()
+        {
+            var random = new Random();
+            var NumberObject = new NumberModel
+            {
+                RandomNumber1 = random.Next(-500, 5000),
+                RandomNumber2 = random.Next(-500, 5000),
+                RandomNumber3 = random.Next(-500, 5000)
+            };
+            return NumberObject;
+        }
+    }
+}
